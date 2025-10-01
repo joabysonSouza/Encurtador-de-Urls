8c238c79f1e99c00b6349ba38d5f2081232f9a5e
Joabyson <joabyson916@gmail.com>
2025-09-30 22:35:43 -0300

    feat: "criando a rota para a captura da  url "

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import express from "express";
+import express, {Request , Response} from "express";
+import { customAlphabet } from "nanoid";
+import db from "./db.js";
 
 const app = express();
 const port = 3000;
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Hello World com Node, Express e TypeScript (ESM)!");
+const nanoid = customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 7);
+
+const insertUrl = db.prepare("INSERT INTO urls (slug,  original_url) VALUES (?, ?)");
+const getBySlug = db.prepare("SELECT * urls WHERE = ? ")
+
+
+app.get("/shorten", (req: Request, res: Response) => {
+   const { url, slug } = req.body;
 });
 
 app.listen(port, () => {
