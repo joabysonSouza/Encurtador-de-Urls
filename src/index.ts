b4693743057c63ef3d5c211c2422b7fbaa5d1f0c
Joabyson <joabyson916@gmail.com>
2025-10-01 23:00:40 -0300

    feat:"criando a parte de post da url no banco de dados e da recuperação da url encurtada  e trabalhando na parte de redirecionamento"

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,56 @@ const app = express();
 const port = 3000;
 app.use(express.json())
 
+interface UrlRow {
+  slug: string;
+  original_url: string;
+}
+
+
+const PORT = process.env.PORT || 3000;
+
 const nanoid = customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 7);
 
 const insertUrl = db.prepare("INSERT INTO urls (slug,  original_url) VALUES (?, ?)");
 const getBySlug = db.prepare("SELECT * urls WHERE = ? ")
 
 
-app.get("/shorten", (req: Request, res: Response) => {
+app.post("/shorten", (req: Request, res: Response) => {
    const { url, slug } = req.body;
+
+   if(!url || typeof url !== "string"){
+     return res.status(400).json( { erro: "Sua Url é invalida"})
+   }
+
+    let finalSlug = slug || nanoid();
+
+    try {
+      insertUrl.run(finalSlug , slug)
+      
+    } catch (error: any) {
+      return res.status(409).json({error : "Slug já exite "})
+      
+    }
+
+    // TODO Não esquecer de setar a as variaveis de ambiente no dotenv
+
+    const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+    const shortUrl = `${BASE_URL}/${finalSlug}`;
+     return res.json({ slug: finalSlug, shortUrl, original: url });
 });
+
+app.get("/:slug", (req: Request, res: Response)=>{
+  const { slug } = req.params
+  const row = getBySlug.get(slug) as UrlRow || undefined
+
+  if(!row){
+     return res.status(404).send("Slug não encontrada ")
+  }
+
+   return res.redirect(row.original_url);
+
+})
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
