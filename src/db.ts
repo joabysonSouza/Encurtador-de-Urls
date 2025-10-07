de3b27da76d1ad1c05a442cce0517750f5ff092d
Joabyson <joabyson916@gmail.com>
2025-10-06 21:59:09 -0300

    refactor: "Concerto sintaxe errada  do SQL"

--- src/db.ts
+++ src/db.ts
@@ -5,11 +5,10 @@ const db = new Database('urls.db');
 db.pragma("journal_mode = WAL");
 
 db.exec(`
-    CREATE TABLE IF NO EXISTS urls(
+    CREATE TABLE IF NOT EXISTS urls(
     slug TEXT PRIMARY KEY,
     original_url TEXT NOT NULL,
-    create_at TEXT DEFAULT (datatime('now'))
-
+    created_at TEXT DEFAULT (datetime('now'))
     ); 
     
   `);
